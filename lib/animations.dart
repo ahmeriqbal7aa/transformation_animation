89799d6df4fffae01bd0976c2bc5fd45906a6450
ahmer7aa <ahmer5253@gmail.com>
2021-02-22 09:54:21 +0500

    Apply Tapped Animation

--- lib/animations.dart
+++ lib/animations.dart
@@ -16,32 +16,79 @@ class _AnimationPageState extends State<AnimationPage>
   void initState() {
     super.initState();
     animationController =
-        AnimationController(duration: Duration(seconds: 5), vsync: this);
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
     // it flying from left side towards center
     // -ve value mean it start from left side
-    animation = IntTween(begin: 0, end: 10).animate(CurvedAnimation(
+    animation = Tween(begin: 0.0, end: -0.15).animate(CurvedAnimation(
       parent: animationController,
-      curve: Curves.easeOut,
+      curve: Curves.ease,
     ));
-
-    animationController.forward();
   }
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return AnimatedBuilder(
       animation: animationController,
       builder: (BuildContext context, Widget child) {
         return Scaffold(
           body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Stack(
               children: [
-                Text('Loading...', style: TextStyle(color: Colors.white)),
-                Text(
-                  animation.value.toString(),
-                  style: TextStyle(fontSize: 50.0, color: Colors.white),
+                Center(
+                  child: Container(
+                    padding: EdgeInsets.all(10.0),
+                    width: 350.0,
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        RaisedButton(
+                          onPressed: () {},
+                          child: Text('Live'),
+                          elevation: 7.0,
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                        ),
+                        RaisedButton(
+                          onPressed: () {},
+                          child: Text('Details'),
+                          elevation: 7.0,
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
                 ),
+                Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      animationController.forward();
+                    },
+                    onDoubleTap: () {
+                      animationController.reverse();
+                    },
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      width: 350.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        image: DecorationImage(
+                          image: AssetImage('images/Makkah.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      transform: Matrix4.translationValues(
+                          0.0, animation.value * width, 0.0),
+                    ),
+                  ),
+                )
               ],
             ),
           ),
